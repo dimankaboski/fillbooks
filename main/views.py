da057a9eb5f2afe7775f56548d3446534707f15c
Aleksei <aleshadev@dimankab1.fvds.ru>
2020-10-21 17:07:15 -0400

    goods list view

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from main.models import Goods
+
 
 # Create your views here.
 def index(request):
@@ -6,6 +9,11 @@ def index(request):
 
 def goods(request):
     return render(request, 'goods.html', context={})
+
+class GoodsListView(ListView):
+
+    template_name = 'goods.html'
+    model = Goods
 
 def login(request):
     return render(request, 'login.html', context={})
